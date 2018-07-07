0c4eac457cbb7e4f141d04dc8e3f78451ceb276d
Andrew Heyman <abheyman23@sbcglobal.net>
2018-07-07 18:16:25 -0400

    Tweaked mnist_pbt_local code style

--- mnist_pbt_local.py
+++ mnist_pbt_local.py
@@ -4,17 +4,15 @@ information at the end.
 """
 
 import datetime
+from tensorflow.models.official.mnist.dataset import train, test
 from pbt import LocalCluster
 from mnist_pbt import ConvNet, plot_hyperparams
-from tensorflow.models.official.mnist.dataset import train, test
 
 
 if __name__ == '__main__':
     train_data = train('MNIST_data/')
     test_data = test('MNIST_data/')
-    pop_size = 50
-    cluster = LocalCluster[ConvNet](pop_size,
-                                    lambda num, sess: ConvNet(num, sess, train_data, test_data))
+    cluster = LocalCluster[ConvNet](50, lambda num, sess: ConvNet(num, sess, train_data, test_data))
     cluster.initialize_variables()
     training_start = datetime.datetime.now()
     cluster.train(20000)
